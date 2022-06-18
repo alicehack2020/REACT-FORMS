f6a7f8f30a9e4da89f3bd957147afedff125becc
Mangesh2042 <mangesh2042@gmail.com>
2022-06-18 17:50:52 +0530

    demo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useState } from 'react';
+// Name
+// Age
+// Address
+// Department ( select tag )
+// Salary
+// marital state ( check box )
+
+
 
 function App() {
+
+  const [Name,setName]=useState("")
+  const [Age,setAge]=useState("")
+  const [Address,setAddress]=useState("")
+  const [Salary,setSalary]=useState("")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <>
+      <div>
+        <h1>Name</h1>
+        <input type="text" value={Name} onChange={(e)=>setName(e.target.value)}/>
+      </div>
+
+      <div>
+        <h1>Age</h1>
+        <input type="Number" value={Age} onChange={(e)=>setAge(e.target.value)}/>
+      </div>
+
+
+      <div>
+        <h1>Address</h1>
+        <input type="text" value={Address} onChange={(e)=>setAddress(e.target.value)}/>
+      </div>
+
+
+      <div>
+        <h1>Department</h1>
+        <select>
+            <option value="grapefruit">Grapefruit</option>
+            <option value="lime">Lime</option>
+            <option selected value="coconut">Coconut</option>
+            <option value="mango">Mango</option>
+        </select>
+      </div>
+
+
+      <div>
+        <h1>Salary</h1>
+        <input type="Number" value={Salary} onChange={(e)=>setSalary(e.target.value)}/>
+      </div>
+
+      <div>
+           <h1>marital state</h1>
+           <input type="checkbox" />
+       </div>
+    
+    
+
+
+     </>
   );
 }
 
